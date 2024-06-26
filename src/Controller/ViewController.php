cd6184e1c3ad97c4df6dd2fab78e3fc6eee461af
KALIN1NA <anya.kalinina.3311@mail.ru>
2024-06-26 18:54:08 +0300

    Склонировала репозиторий, добавила файлы Андрея, которые отвечают за логику хождения персоонажа, библиотека pixi.js

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -9,24 +9,29 @@ use Symfony\Component\HttpFoundation\Response;
 class ViewController extends AbstractController
 {
 
-	public function index(): Response
-	{
-		return $this->render('screensaver.html.twig');
-	}
+    public function index(): Response
+    {
+        return $this->render('screensaver.html.twig');
+    }
 
-	public function signUpUserForm(): Response
-	{
-		return $this->render('signup-user-form.html.twig');
-	}
+    public function signUpUserForm(): Response
+    {
+        return $this->render('signup-user-form.html.twig');
+    }
 
-	public function signInUserForm(): Response
-	{
-		return $this->render('signin-user-form.html.twig');
-	}
+    public function signInUserForm(): Response
+    {
+        return $this->render('signin-user-form.html.twig');
+    }
 
-	public function showMenu(): Response
-	{
-		return $this->render('menu.html.twig');
-	}
+    public function showMenu(): Response
+    {
+        return $this->render('menu.html.twig');
+    }
+
+    public function showGame(): Response
+    {
+        return $this->render('game.html.twig');
+    }
 
 }
