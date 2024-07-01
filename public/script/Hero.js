222faf86035fa29f7663b909f74ab62c504c035a
Ersulov <ersulov05@mail.ru>
2024-07-01 17:09:48 +0300

    andrei-06-28-animation-person Правки по review перенес все "{" на новую строку, везде поставил ";", изменил названия картинок через нижнее подчёркивание.

--- public/script/Hero.js
+++ public/script/Hero.js
@@ -1,229 +1,275 @@
-class Hero {
-    constructor(texture, posX, posY, speedX, speedY) {
+class Hero
+{
+    constructor(posX, posY, speedX, speedY)
+    {
         this.sprite = new PIXI.AnimatedSprite(hero_idle);
 
         this.sprite.animationSpeed = 0.2; // Скорость анимации
         this.sprite.loop = true; // Зацикливание анимации
         this.sprite.play(); // Запуск анимации
 
-        this.sprite.x = posX
-        this.sprite.y = posY
-        this.sprite.width = 60
-        this.sprite.height = 85
+        this.sprite.x = posX;
+        this.sprite.y = posY;
+        this.sprite.width = 60;
+        this.sprite.height = 85;
 
-        this.collideTop = this.sprite.y - this.sprite.height / 2
-        this.collideBottom = this.sprite.y + this.sprite.height / 2
-        this.collideLeft = this.sprite.x - this.sprite.width / 2
-        this.collideRight = this.sprite.x + this.sprite.width / 2
+        this.collideTop = this.sprite.y - this.sprite.height / 2;
+        this.collideBottom = this.sprite.y + this.sprite.height / 2;
+        this.collideLeft = this.sprite.x - this.sprite.width / 2;
+        this.collideRight = this.sprite.x + this.sprite.width / 2;
 
-        this.sprite.anchor.set(0.5)
-        this.speedX = speedX
-        this.speedY = speedY
-        this.sprite.vx = 0
-        this.sprite.vy = 1
-        this.isGround = false
-        this.cameraRectX = 100
-        this.cameraRectY = 10
+        this.sprite.anchor.set(0.5);
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.sprite.vx = 0;
+        this.sprite.vy = 1;
+        this.isGround = false;
+        this.cameraRectX = 100;
+        this.cameraRectY = 10;
 
-        this.jumpPower = 12
-        this.doubleJump = true
-        this.gravitationPower = 0.5
-        this.weaponTime = 10
-        this.weaponCountBullet = 20
-        this.currentWeaponTime = 0
-        this.currentCountBullet = this.weaponCountBullet
-        this.rechargeTime = 300
-        this.currentRechargeTime = this.rechargeTime
-        this.ainimateType = ''
+        this.jumpPower = 12;
+        this.doubleJump = true;
+        this.gravitationPower = 0.5;
+        this.weaponTime = 10;
+        this.weaponCountBullet = 20;
+        this.currentWeaponTime = 0;
+        this.currentCountBullet = this.weaponCountBullet;
+        this.rechargeTime = 300;
+        this.currentRechargeTime = this.rechargeTime;
+        this.ainimateType = '';
 
-
-
-        let radius = 300
-        let blurSize = 50
+        let radius = 300;
+        let blurSize = 50;
         const circle = new PIXI.Graphics().circle(radius + blurSize, radius + blurSize, radius).fill({ color: 0xff0000 });
-
         circle.filters = [new PIXI.BlurFilter(blurSize)];
-
         const bounds = new PIXI.Rectangle(0, 0, (radius + blurSize) * 2, (radius + blurSize) * 2);
-        const texture1 = app.renderer.generateTexture({
-            target: circle,
-            style: { scaleMode: PIXI.SCALE_MODES.NEAREST },
-            resolution: 1,
-            frame: bounds,
-        });
+        const texture1 = app.renderer.generateTexture(
+            {
+                target: circle,
+                style: { scaleMode: PIXI.SCALE_MODES.NEAREST },
+                resolution: 1,
+                frame: bounds,
+            }
+        );
         this.focusTexture = new PIXI.Sprite(texture1);
         this.focusTexture.tint = 0x000000;
-        this.focusTexture.anchor.set(0.5)
+        this.focusTexture.anchor.set(0.5);
 
-
-
-        this.updateAnim = function (type) {
-            if (type == 'idle') {
+        this.updateAnim = function (type)
+        {
+            if (type == 'idle')
+            {
                 this.sprite.loop = false;
                 this.sprite.textures = hero_idle;
                 this.sprite.animationSpeed = 0.15;
                 this.sprite.loop = true;
                 this.sprite.play();
-                this.ainimateType = 'idle'
-            } else if (type == 'walk') {
+                this.ainimateType = 'idle';
+            } else if (type == 'walk')
+            {
                 this.sprite.textures = hero_walk;
                 this.sprite.animationSpeed = 0.3;
                 this.sprite.loop = true;
                 this.sprite.play();
-                this.ainimateType = 'walk'
-            } else if (type == 'jump') {
+                this.ainimateType = 'walk';
+            } else if (type == 'jump')
+            {
                 this.sprite.textures = hero_jump;
                 this.sprite.animationSpeed = 0.3;
                 this.sprite.loop = false;
                 this.sprite.play();
-                this.ainimateType = 'jump'
+                this.ainimateType = 'jump';
             }  
         }
 
-        this.updateCollide = function () {
-            this.collideTop = this.sprite.y - this.sprite.height / 2
-            this.collideBottom = this.sprite.y + this.sprite.height / 2 - 6
-            this.collideLeft = this.sprite.x - this.sprite.width / 2 + 10
-            this.collideRight = this.sprite.x + this.sprite.width / 2 - 10
+        this.updateCollide = function ()
+        {
+            this.collideTop = this.sprite.y - this.sprite.height / 2;
+            this.collideBottom = this.sprite.y + this.sprite.height / 2 - 6;
+            this.collideLeft = this.sprite.x - this.sprite.width / 2 + 10;
+            this.collideRight = this.sprite.x + this.sprite.width / 2 - 10;
         }
 
-        this.createBullet = function (mouseX, mouseY) {
-            if (this.currentWeaponTime <= 0 && this.currentCountBullet > 0) {
-                let globalPosition = this.sprite.getGlobalPosition()
-                let distance = Math.sqrt((globalPosition.x - mouseX)*(globalPosition.x - mouseX) + (globalPosition.y - mouseY)*(globalPosition.y - mouseY))
-                let vecX = (mouseX - globalPosition.x) / distance
-                if ((this.sprite.vx > 0 && vecX >= 0) || (this.sprite.vx < 0 && vecX <= 0) || (this.sprite.vx == 0)){
-                    let vecY = (mouseY - globalPosition.y) / distance
-                    let angle = Math.acos(vecX)
-                    if (vecY < 0) {
-                        angle *= -1
+        this.createBullet = function (mouseX, mouseY)
+        {
+            if (this.currentWeaponTime <= 0 && this.currentCountBullet > 0)
+            {
+                let globalPosition = this.sprite.getGlobalPosition();
+                let distance = Math.sqrt((globalPosition.x - mouseX) * (globalPosition.x - mouseX) + (globalPosition.y - mouseY) * (globalPosition.y - mouseY));
+                let vecX = (mouseX - globalPosition.x) / distance;
+                if ((this.sprite.vx > 0 && vecX >= 0) || (this.sprite.vx < 0 && vecX <= 0) || (this.sprite.vx == 0))
+                {
+                    let vecY = (mouseY - globalPosition.y) / distance;
+                    let angle = Math.acos(vecX);
+                    if (vecY < 0)
+                    {
+                        angle *= -1;
                     }
-                    let bullet = new Bullet('bullet', this.sprite.x, this.sprite.y, vecX, vecY, angle)
-                    this.currentCountBullet -= 1
-                    if (this.currentRechargeTime)
-                    this.currentWeaponTime = this.weaponTime
-                    bullet.view()
-                    bullets.push(bullet)
-                    if (vecX > 0 && this.sprite.scale.x < 0) {
-                        this.sprite.scale.x *= -1
+                    let bullet = new Bullet('bullet', this.sprite.x, this.sprite.y, vecX, vecY, angle);
+                    this.currentCountBullet -= 1;
+                    this.currentWeaponTime = this.weaponTime;
+                    bullet.view();
+                    bullets.push(bullet);
+                    if (vecX > 0 && this.sprite.scale.x < 0)
+                    {
+                        this.sprite.scale.x *= -1;
                     }
-                    if (vecX < 0 && this.sprite.scale.x > 0) {
-                        this.sprite.scale.x *= -1
+                    if (vecX < 0 && this.sprite.scale.x > 0)
+                    {
+                        this.sprite.scale.x *= -1;
                     }
                 }
             }          
         }
 
-        this.view = function () {
-            
+        this.view = function ()
+        {
             scene.addChild(this.focusTexture);
             scene.addChild(this.sprite);
             scene.mask = this.focusTexture;
         }
 
-        this.deleteView = function () {
+        this.deleteView = function ()
+        {
             scene.removeChild(this.sprite);
         }
-        this.updateWeapon = function (time) {
-            if (this.currentCountBullet > 0) {
-                if (this.currentWeaponTime > 0) {
-                    this.currentWeaponTime -= time.deltaTime
+        this.updateWeapon = function (time)
+        {
+            if (this.currentCountBullet > 0)
+            {
+                if (this.currentWeaponTime > 0)
+                {
+                    this.currentWeaponTime -= time.deltaTime;
                 }
-            } else {
-                if (this.currentRechargeTime > 0) {
-                    this.currentRechargeTime -= time.deltaTime
-                } else {
-                    this.currentWeaponTime = 0
-                    this.currentCountBullet = this.weaponCountBullet
-                    this.currentRechargeTime = this.rechargeTime
+            }
+            else
+            {
+                if (this.currentRechargeTime > 0)
+                {
+                    this.currentRechargeTime -= time.deltaTime;
+                }
+                else
+                {
+                    this.currentWeaponTime = 0;
+                    this.currentCountBullet = this.weaponCountBullet;
+                    this.currentRechargeTime = this.rechargeTime;
                 }
             }  
         }
-        this.update = function (time) {
-            //console.log(this.ainimateType)
-            this.focusTexture.x = this.sprite.x
-            this.focusTexture.y = this.sprite.y
+        this.updateKey = function() 
+        {
             if (keys.keyLeft) {
-                this.sprite.vx = -this.speedX
+                this.sprite.vx = -this.speedX;
                 if (this.sprite.scale.x > 0)
                 {
                     this.sprite.scale.x *= -1
                 } 
             }
-            if (keys.keyRight) {
-                this.sprite.vx = this.speedX
+            if (keys.keyRight)
+            {
+                this.sprite.vx = this.speedX;
                 if (this.sprite.scale.x < 0)
                 {
-                    this.sprite.scale.x *= -1
+                    this.sprite.scale.x *= -1;
                 } 
             }
-            if (!keys.keyLeft && !keys.keyRight) {
-                this.sprite.vx = 0
+            if (!keys.keyLeft && !keys.keyRight)
+            {
+                this.sprite.vx = 0;
             }
             if (keys.keyUp) {
-                if (this.isGround) {
-                    this.updateAnim('jump')
-                    this.sprite.vy = -this.jumpPower
-                } else {
-                    if (this.doubleJump && this.sprite.vy > 0) {
-                        this.updateAnim('jump')
-                        this.sprite.vy = -this.jumpPower
-                        this.doubleJump = false
+                if (this.isGround)
+                {
+                    this.updateAnim('jump');
+                    this.sprite.vy = -this.jumpPower;
+                }
+                else
+                {
+                    if (this.doubleJump && this.sprite.vy > 0)
+                    {
+                        this.updateAnim('jump');
+                        this.sprite.vy = -this.jumpPower;
+                        this.doubleJump = false;
                     }
                 }
             }
-
-            if (!this.isGround) {
-                this.sprite.vy += this.gravitationPower * time.deltaTime
-            } else {
-                if (this.sprite.vy > 0) {
-                    this.sprite.vy = 0   
+        }
+        this.updateMove = function(time) 
+        {
+            if (!this.isGround)
+            {
+                this.sprite.vy += this.gravitationPower * time.deltaTime;
+            }
+            else
+            {
+                if (this.sprite.vy > 0)
+                {
+                    this.sprite.vy = 0;   
                 }  
-                if (!keys.keyLeft && !keys.keyRight) {
-                    this.sprite.vx = 0
-                    if (this.ainimateType !== 'idle' && this.sprite.vy >= 0) {
-                        this.updateAnim('idle')
+                if (!keys.keyLeft && !keys.keyRight)
+                {
+                    this.sprite.vx = 0;
+                    if (this.ainimateType !== 'idle' && this.sprite.vy >= 0)
+                    {
+                        this.updateAnim('idle');
                     }
-                } else {
-                    if (this.ainimateType !== 'walk' && this.sprite.vy >= 0){
-                        this.updateAnim('walk')
+                }
+                else
+                {
+                    if (this.ainimateType !== 'walk' && this.sprite.vy >= 0)
+                    {
+                        this.updateAnim('walk');
                     } 
                 }
             }
-            this.sprite.x += this.sprite.vx * time.deltaTime
-            this.sprite.y += this.sprite.vy * time.deltaTime
+            this.sprite.x += this.sprite.vx * time.deltaTime;
+            this.sprite.y += this.sprite.vy * time.deltaTime;
+            this.focusTexture.x = this.sprite.x;
+            this.focusTexture.y = this.sprite.y;
             
-            let globalPosition = this.sprite.getGlobalPosition()
-            let deltaX = globalPosition.x - app.screen.width / 2 
-            if (deltaX > this.cameraRectX && this.sprite.scale.x > 0) {
-                let moveX = deltaX - this.cameraRectX
-                moveCamera(moveX, 0)
+            let globalPosition = this.sprite.getGlobalPosition();
+            let deltaX = globalPosition.x - app.screen.width / 2; 
+            if (deltaX > this.cameraRectX && this.sprite.scale.x > 0)
+            {
+                let moveX = deltaX - this.cameraRectX;
+                moveCamera(moveX, 0);
             }
 
-            if (deltaX < -this.cameraRectX && this.sprite.scale.x < 0) {
-                let moveX = deltaX + this.cameraRectX
-                moveCamera(moveX, 0)
+            if (deltaX < -this.cameraRectX && this.sprite.scale.x < 0)
+            {
+                let moveX = deltaX + this.cameraRectX;
+                moveCamera(moveX, 0);
             }
-            this.updateWeapon(time)
-            this.collise()
+        }
+        this.update = function (time)
+        {
+            this.updateKey();
+            this.updateMove(time);
+            this.updateWeapon(time);
+            this.collise();
         }
 
-        this.collise = function () {
-            this.isGround = false
+        this.collise = function ()
+        {
+            this.isGround = false;
             if (this.sprite.vy >= 0) {
-                this.updateCollide()
-                platforms.forEach(platform => {
+                this.updateCollide();
+                platforms.forEach(platform =>
+                {
                     if (this.collideBottom <= platform.collideBottom + this.sprite.vy &&
                         this.collideBottom >= platform.collideTop &&
                         this.collideLeft <= platform.collideRight &&
                         this.collideRight >= platform.collideLeft
-                    ) {
-                        this.isGround = true
-                        this.doubleJump = true
-                        if (this.sprite.vy > 1) {
-                            this.sprite.y -= (this.collideBottom - platform.collideTop)
-                        } else {
-                            this.sprite.y -= (this.collideBottom - platform.collideTop) / 4
+                    )
+                    {
+                        this.isGround = true;
+                        this.doubleJump = true;
+                        if (this.sprite.vy > 1)
+                        {
+                            this.sprite.y -= (this.collideBottom - platform.collideTop);
+                        } else
+                        {
+                            this.sprite.y -= (this.collideBottom - platform.collideTop) / 4;
                         }     
                     }
                 })
--- public/script/index.js
+++ public/script/index.js
@@ -10,100 +10,117 @@ let background
 const hero_walk = []
 const hero_jump = []
 const hero_idle = []
-const keys = {
+const keys =
+{
     keyDown: false,
     keyUp: false,
     keyLeft: false,
     keyRight: false,
 }
-const mouse = {
+const mouse =
+{
     isDownLeft: false,
     positionX: 0,
     positionY: 0,
 }
-function onAppMouseDown(event) {
-    if (event.button === 0) {
-        mouse.isDownLeft = true
+function onAppMouseDown(event)
+{
+    if (event.button === 0)
+    {
+        mouse.isDownLeft = true;
     }
 }
-function onAppMouseMove(event) {
-    if (event.button === 0) {
-        mouse.positionX = event.clientX
-        mouse.positionY = event.clientY
+function onAppMouseMove(event)
+{
+    if (event.button === 0)
+    {
+        mouse.positionX = event.clientX;
+        mouse.positionY = event.clientY;
     }
 }
-function onAppMouseUp(event) {
-    if (event.button === 0) {
-        mouse.isDownLeft = false
+function onAppMouseUp(event)
+{
+    if (event.button === 0)
+    {
+        mouse.isDownLeft = false;
     }
 }
 
-function onKeyDown(event) {
-    if (event.keyCode === 37) {
-        keys.keyLeft = true
-        keys.keyRight = false
+function onKeyDown(event)
+{
+    if (event.keyCode === 37)
+    {
+        keys.keyLeft = true;
+        keys.keyRight = false;
     }
-    if (event.keyCode === 39) {
-        keys.keyRight = true
-        keys.keyLeft = false
+    if (event.keyCode === 39)
+    {
+        keys.keyRight = true;
+        keys.keyLeft = false;
     }
-    if (event.keyCode === 38) {
-        keys.keyUp = true
-        keys.keyDown = false
+    if (event.keyCode === 38)
+    {
+        keys.keyUp = true;
+        keys.keyDown = false;
     }
-    if (event.keyCode === 40) {
-        keys.keyDown = true
-        keys.keyUp = false
+    if (event.keyCode === 40)
+    {
+        keys.keyDown = true;
+        keys.keyUp = false;
     }
 }
-function onKeyUp(event) {
-    if (event.keyCode === 37) {
-        keys.keyLeft = false
+function onKeyUp(event)
+{
+    if (event.keyCode === 37)
+    {
+        keys.keyLeft = false;
     }
-    if (event.keyCode === 39) {
-        keys.keyRight = false
+    if (event.keyCode === 39)
+    {
+        keys.keyRight = false;
     }
-    if (event.keyCode === 40) {
-        keys.keyDown = false
+    if (event.keyCode === 40)
+    {
+        keys.keyDown = false;
     }
-    if (event.keyCode === 38) {
-        keys.keyUp = false
+    if (event.keyCode === 38)
+    {
+        keys.keyUp = false;
     }
 }
 
 (async () =>
 {
-    // Intialize the application.
-    await app.init({ background: '#000000',  resizeTo: window });//
-
-    // Then adding the application's canvas to the DOM body.
+    await app.init({ background: '#000000',  resizeTo: window });
     document.body.appendChild(app.canvas);
 
     await PIXI.Assets.load([
         { alias: 'background', src: '/images/level1-map.jpg' },
-        { alias: 'hero-idle-group', src: '/images/hero-idle-group.json' },
-        { alias: 'hero-walk-group', src: '/images/hero-walk-group.json' },
-        { alias: 'hero-jump-group', src: '/images/hero-jump-group.json' },        
+        { alias: 'hero_walk_group', src: '/images/hero_walk_group.json' },
+        { alias: 'hero_idle_group', src: '/images/hero_idle_group.json' },
+        { alias: 'hero_jump_group', src: '/images/hero_jump_group.json' },        
         { alias: 'hero', src: '/images/hero.svg' },
         { alias: 'ground', src: '/images/ground.svg' },
         { alias: 'bullet', src: '/images/bullet.svg' },
     ])
-    for (let i = 0; i < 10; i++) {
-        hero_idle.push(PIXI.Texture.from(`idle${1 + i}.png`))
+    for (let i = 0; i < 10; i++)
+    {
+        hero_idle.push(PIXI.Texture.from(`idle${1 + i}.png`));
     }
-    for (let i = 0; i < 10; i++) {
-        hero_walk.push(PIXI.Texture.from(`walk${1 + i}.png`))
+    for (let i = 0; i < 10; i++)
+    {
+        hero_walk.push(PIXI.Texture.from(`walk${1 + i}.png`));
     }
-    for (let i = 0; i < 10; i++) {
-        hero_jump.push(PIXI.Texture.from(`jump${1 + i}.png`))
+    for (let i = 0; i < 10; i++)
+    {
+        hero_jump.push(PIXI.Texture.from(`jump${1 + i}.png`));
     }
 
-
-    background = PIXI.Sprite.from('background');
+    const background = PIXI.Sprite.from('background');
     background.anchor.set(0);
 
-
-    function resizeBackground() {
+    function resizeBackground()
+    {
         background.width = app.screen.width;
         background.height = app.screen.height;
     }
@@ -111,66 +128,72 @@ function onKeyUp(event) {
     resizeBackground();
     scene.addChild(background);
 
-
-
-
-    const hero = new Hero('hero',100, 100, 6, 0)
-
-    app.canvas.addEventListener('mousedown', onAppMouseDown)
-    app.canvas.addEventListener('mousemove', onAppMouseMove)
-    app.canvas.addEventListener('mouseup', onAppMouseUp)
-    levelCreate()
-    
-    platforms.forEach(platform => {
-        platform.view()
-    })
-
+    app.canvas.addEventListener('mousedown', onAppMouseDown);
+    app.canvas.addEventListener('mousemove', onAppMouseMove);
+    app.canvas.addEventListener('mouseup', onAppMouseUp);
+    levelCreate();
     app.stage.addChild(scene);
-
-    hero.view()
-    app.ticker.add((time) => {
-        hero.update(time)
-        if (mouse.isDownLeft) {
-            hero.createBullet(mouse.positionX, mouse.positionY)
+    const hero = new Hero(100, 100, 6, 0);
+    hero.view();
+    app.ticker.add((time) =>
+    {
+        hero.update(time);
+        if (mouse.isDownLeft)
+        {
+            hero.createBullet(mouse.positionX, mouse.positionY);
         }
-        let i = 0
-
-        while (i < bullets.length) {
-            if (bullets[i].lifeTime > 0) {
-                bullets[i].update(time)
-            } else {
-                bullets[i].sprite.destroy()
-                bullets.splice(i, 1)
+        let i = 0;
+        while (i < bullets.length)
+        {
+            if (bullets[i].lifeTime > 0)
+            {
+                bullets[i].update(time);
             }
-            i++
+            else
+            {
+                bullets[i].sprite.destroy();
+                bullets.splice(i, 1);
+            }
+            i++;
         }
     });
 })();
 
-function moveCamera(x, y) {
-    let moveX = x
-    let moveY = y
-    if (scene.x - x > 0) {
-        moveX = scene.x
-        scene.x -= moveX
-    } else if (scene.x - x < app.screen.width - SCENE_WIDTH) {
-        moveX = (scene.x + SCENE_WIDTH - app.screen.width)
-        scene.x -= moveX
-    } else {
-        scene.x -= moveX
+function moveCamera(x, y)
+{
+    let moveX = x;
+    let moveY = y;
+    if (scene.x - x > 0)
+    {
+        moveX = scene.x;
+        scene.x -= moveX;
     }
-    scene.y += y
+    else if (scene.x - x < app.screen.width - SCENE_WIDTH)
+    {
+        moveX = (scene.x + SCENE_WIDTH - app.screen.width);
+        scene.x -= moveX;
+    }
+    else
+    {
+        scene.x -= moveX;
+    }
+    scene.y += y;
 }
 
 window.addEventListener('keydown', onKeyDown)
 window.addEventListener('keyup', onKeyUp)
 
-function levelCreate() {
-    let texture = PIXI.Texture.from('ground')
-    platforms.push(new Ground(texture, 89, 250, 178, 40))
-    platforms.push(new Ground(texture, 300, 400, 178, 40))
-    platforms.push(new Ground(texture, 300, 300, 178, 40))
-    platforms.push(new Ground(texture, 500, 400, 178, 40))
-    platforms.push(new Ground(texture, 100, 500, 178, 40))
+function levelCreate()
+{
+    let texture = PIXI.Texture.from('ground');
+    platforms.push(new Ground(texture, 89, 250, 178, 40));
+    platforms.push(new Ground(texture, 300, 400, 178, 40));
+    platforms.push(new Ground(texture, 300, 300, 178, 40));
+    platforms.push(new Ground(texture, 500, 400, 178, 40));
+    platforms.push(new Ground(texture, 100, 500, 178, 40));
+    platforms.forEach(platform =>
+    {
+        platform.view();
+    })
 }
 
