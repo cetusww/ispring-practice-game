b6d1b5cfadaa233da62dacd7d056f2b0a2e2adad
cetusw <kugelev.m@gmail.com>
2024-07-11 14:49:22 +0300

    2024-07-13-dev обновил базу данных

--- data/user.sql
+++ data/user.sql
@@ -7,5 +7,9 @@ CREATE TABLE user
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    level int DEFAULT NULL,
+    score_1 int DEFAULT NULL,
+    score_2 int DEFAULT NULL,
+    score_3 int DEFAULT NULL,
     UNIQUE (username)
 );
