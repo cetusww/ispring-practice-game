5762d6814c71caa6c03a2bcae6c4e6ba34141fb2
pkluzhev <pkluzhev1901@gmail.com>
2024-07-16 10:34:27 +0300

    pavel-07-11-boss-level3  Добавлены файлы босса и его снарядов

---
+++ public/script/Boss.js
@@ -0,0 +1,373 @@
+class Boss
+{
+    constructor(posX, posY, zoneWidth)
+    {
+        this.sprite = new PIXI.AnimatedSprite(boss_idle);
+        this.sprite.animationSpeed = 0.1; // Скорость анимации
+        this.sprite.loop = true;          // Зацикливание анимации
+        this.sprite.play();               // Запуск анимации
+
+        this.sprite.x = posX;
+        this.sprite.y = posY;
+        this.sprite.width = 96;
+        this.sprite.height = 160;
+
+        this.collideTop = this.sprite.y - this.sprite.height / 2;
+        this.collideBottom = this.sprite.y + this.sprite.height / 2;
+        this.collideLeft = this.sprite.x - this.sprite.width / 2;
+        this.collideRight = this.sprite.x + this.sprite.width / 2;
+
+        this.sprite.anchor.set(0.5);
+        this.sprite.vx = 2.5;
+
+        this.zoneWidth = zoneWidth;
+
+        this.zoneX = posX;
+        this.zoneY = posY;
+
+        this.shotSpeed = 7;
+
+        this.timeShotAttack = 100;      //частота генерации одиночных выстрелов
+        this.currentTimeShotAttack = 0;
+
+        this.timeUltaAttack = 800;      //частота генерации ульты (веера из выстрелов)
+        this.currentTimeUltaAttack = 800;
+ 
+        // this.timeSpawnDevils = 100;     //частота спавна демонов   //сделать спавн тремя волнами в зависимости от хп босса wawe1, wwavw2. wawe 3 boolean
+        // this.currentTimeSpawnDevils = 100;
+
+        // let devilZoneWidth = 100;        //зона передвижения для демонов
+        // let devilZoneHeight = 0;
+
+        // let wave1 = false;        //флаги для трёх волн спавна демонов
+        // let wave2 = false;
+        // let wave3 = false;
+
+        // const devilSpawnPlace = {       // точки спавна демонов
+        //     1: {
+        //         x:1200,
+        //         y:500,
+        //         // engaged: false,
+        //     },
+        //     2: {
+        //         x:1600,
+        //         y:500,
+        //         // engaged: false,
+        //     },
+        //     3: {
+        //         x:800,
+        //         y:500,
+        //         // engaged: false,
+        //     },
+        // };
+
+        this.maxHp = 100;
+        this.hp = 100;
+
+        this.animateType = 'idle';
+        this.deadTime = 3 * FPS;
+
+        this.state = 0; // 0 - стоять, 1 - ходить
+        this.stateTime = Math.random() * FPS;
+        
+        this.view = function ()
+        {
+            scene.addChild(this.sprite);
+            this.updateHp();
+        }
+
+        this.deleteView = function ()
+        {
+            scene.removeChild(this.sprite);
+            scene.removeChild(this.graphics);
+        }
+
+        this.updateHp = function () {
+            scene.removeChild(this.graphics);
+            this.graphics = new PIXI.Graphics();
+            this.graphics.rect(this.sprite.x - this.sprite.width / 2 + 5, this.sprite.y - this.sprite.height / 2 - 7, this.hp / this.maxHp * (this.sprite.width - 10), 5);
+            this.graphics.fill(0xde3249);
+            this.graphics.rect(this.sprite.x - this.sprite.width / 2 + 5, this.sprite.y - this.sprite.height / 2 - 7, this.sprite.width - 10, 5);
+            this.graphics.stroke({ width: 1, color: 0xfeeb77 });
+            scene.addChild(this.graphics);
+        }
+
+        this.updateAnim = function (type) {
+            if (type === 'boss_idle' && this.animateType !== 'boss_idle') {
+                this.sprite.loop = false;
+                this.sprite.textures = boss_idle;
+                this.sprite.animationSpeed = 0.05;
+                this.sprite.loop = true;
+                this.sprite.play();
+                this.animateType = 'boss_idle';
+            } else if (type === 'boss_walk' && this.animateType !== 'boss_walk') {
+                this.sprite.textures = boss_walk;
+                this.sprite.animationSpeed = 0.25;
+                this.sprite.loop = true;
+                this.sprite.play();
+                this.animateType = 'boss_walk';
+            } else if (type === 'boss_dead' && this.animateType !== 'boss_dead') {
+                this.sprite.textures = boss_dead;
+                this.sprite.animationSpeed = 0.05;
+                this.sprite.loop = false;
+                this.sprite.play();
+                this.animateType = 'boss_dead';
+            }
+        }
+
+        this.updateCollide = function ()
+        {
+            this.collideTop = this.sprite.y - this.sprite.height / 2;
+            this.collideBottom = this.sprite.y + this.sprite.height / 2;
+            this.collideLeft = this.sprite.x - this.sprite.width / 2;
+            this.collideRight = this.sprite.x + this.sprite.width / 2;
+        }
+
+        this.createShot = function (heroX, heroY)
+        {
+            if (this.currentTimeShotAttack <= 0)
+            {
+                let shotSpeed = this.shotSpeed
+                let distanceX = heroX - this.sprite.x;
+                let distanceY = heroY - this.sprite.y;
+                let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+                let angleCos = distanceX / distance;
+                let angleSin = distanceY / distance;
+                let vecX = Math.abs(shotSpeed * angleCos);
+                let vecY = Math.abs(shotSpeed * angleSin);
+                if (heroX > this.sprite.x && heroY < this.sprite.y)
+                {
+                    vecY *= -1;
+                }
+                if (heroX < this.sprite.x && heroY > this.sprite.y)
+                {
+                    vecX *= -1;
+                }
+                if (heroX < this.sprite.x && heroY < this.sprite.y)
+                {
+                    vecX *= -1;
+                    vecY *= -1;
+                }
+                const shot = new Shot('shot', this.sprite.x, this.sprite.y, vecX, vecY, 1);
+                shot.view();
+                shots.push(shot);
+
+                let audio = new Audio('/sounds/vyistrel.ogg');
+                audio.volume = 0.2;
+                audio.load();
+                audio.play();
+
+                this.currentTimeShotAttack = this.timeShotAttack;
+            }  
+        }
+
+        this.createUlta = function ()
+        {
+            if (this.currentTimeUltaAttack <= 0) 
+            {
+                let shotSpeed = this.shotSpeed
+                let deg;
+
+                for(deg = 10; deg <= 170; deg += 20)   //генерация веера из фаерболлов в полуокружности с шагом в 20 градусов
+                {
+                    let angleCos = Math.cos(deg * Math.PI / 180);
+                    let angleSin = - Math.sin(deg * Math.PI / 180);
+                    let vecX = shotSpeed * angleCos;
+                    let vecY = shotSpeed * angleSin;
+
+                    const shot = new Shot('shot', this.sprite.x, this.sprite.y, vecX, vecY, 1);
+                    shot.view();
+                    shots.push(shot);
+                }
+
+                let audio = new Audio('/sounds/vyistrel.ogg');
+                audio.volume = 0.2;
+                audio.load();
+                audio.play();
+
+                this.currentTimeUltaAttack = this.timeUltaAttack;
+            }  
+        }
+
+        // this.spawnDevils = function ()
+        // {
+        //     let q = Math.floor(Math.random() * 10 / 4 + 1);  // принимает рандомные значения с 1 по 3
+            
+        //     for(let c = 0; c < q; c++)
+        //     {
+        //         let spawned = false;
+        //         while (spawned === false)
+        //         {
+        //             let placeNum = Math.floor(Math.random() * 10 / 4 + 1); // принимает рандомные значения с 1 по 3
+        //             if (devilSpawnPlace[placeNum].engaged === false)
+        //             {
+        //                 const devil = new Devil(devilSpawnPlace[placeNum].x, devilSpawnPlace[placeNum].y, devilZoneWidth, devilZoneHeight, 500, 50, 130);
+        //                 devil.view();
+        //                 enemies.push(devil);
+        //                 devilSpawnPlace[placeNum].engaged = true;
+        //                 spawned = true;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // this.spawnDevils = function ()
+        // {
+        //     // let q = Math.floor(Math.random() * 10 / 4 + 1);  // принимает рандомные значения с 1 по 3
+        //     for(let placeNum = 1; placeNum < 4; c++)
+        //     {
+        //         const devil = new Devil(devilSpawnPlace[placeNum].x, devilSpawnPlace[placeNum].y, devilZoneWidth, devilZoneHeight, 500, 50, 130);
+        //         devil.view();
+        //         enemies.push(devil);
+        //     }
+        // }
+
+        // this.spawnDevils = function ()
+        // {
+        //     if (this.currentTimeSpawnDevils <= 0)
+        //     {
+        //         let spawned = false;
+        //         while (spawned === false)
+        //         {
+        //             let placeNum = Math.floor(Math.random() * 10 / 4 + 1); // принимает рандомные значения с 1 по 3
+        //             if (devilSpawnPlace[placeNum].engaged === false)
+        //             {
+        //                 const devil = new Devil(devilSpawnPlace[placeNum].x, devilSpawnPlace[placeNum].y, devilZoneWidth, devilZoneHeight, 500, 50, 130);
+        //                 devil.view();
+        //                 enemies.push(devil);
+        //                 devilSpawnPlace[placeNum].engaged = true;
+        //                 spawned = true;
+        //             }
+        //         }   
+        //     }  
+        // }
+
+        this.takeDamage = function (damage)
+        {
+            this.hp -= damage;
+            if (this.hp <= 0) 
+            {
+                this.hp = 0;
+
+                this.updateAnim('boss_dead');
+
+                this.dead = true;
+            }
+            this.updateHp();
+        }
+
+        this.updateMove = function(time) 
+        { 
+            if (this.stateTime > 0)
+            {
+                this.stateTime -= time.deltaTime;
+            }
+            else
+            {
+                if (Math.random() > 0.4) 
+                {
+                    this.state = 1;
+                    this.stateTime = (Math.random() + 1) * 2 * FPS;
+                    this.updateAnim('boss_walk');
+                } 
+                else
+                {
+                    this.state = 0;
+                    this.stateTime = (Math.random() + 0.5) * FPS;
+                    this.updateAnim('boss_idle');
+                }
+            }
+            if (this.state === 1)
+            {
+                this.sprite.x += this.sprite.vx * time.deltaTime;
+                this.updateCollide();
+                if (this.collideLeft <= this.zoneX - this.zoneWidth)
+                {
+                    this.sprite.vx *= -1;
+                    this.sprite.x += (this.zoneX - this.zoneWidth) - this.collideLeft;
+                }
+                else if (this.collideRight >= this.zoneX + this.zoneWidth)
+                {
+                    this.sprite.vx *= -1;
+                    this.sprite.x -= this.collideRight - (this.zoneX + this.zoneWidth);
+                }
+            }
+            if ((this.sprite.scale.x < 0 && this.sprite.vx > 0) || (this.sprite.scale.x > 0 && this.sprite.vx < 0))
+            {
+                this.sprite.scale.x *= -1;
+            }
+        }
+
+        this.updateAggression = function ()
+        {
+            if (!hero.dead)
+            {
+                this.createUlta();
+                if (this.currentTimeUltaAttack <= 700) 
+                {
+                    this.createShot(hero.sprite.x, hero.sprite.y);
+                }      
+                // this.spawnDevils();        
+                // if (this.hp <= 0.98 * this.maxHp && wave1 === false)
+                // {
+                //     this.spawnDevils();
+                //     wave1 === true;
+                // } 
+                // else if (this.hp <= 0.64 * this.maxHp && wave2 === false)
+                // {
+                //     this.spawnDevils();
+                //     wave2 === true;
+                // }
+                // else if (this.hp <= 0.31 * this.maxHp && wave3 === false)
+                // {
+                //     this.spawnDevils();
+                //     wave3 === true;
+                // }             
+            }
+        }
+
+        this.dropExperience = function ()
+        {    
+            let randomCount = Math.floor(Math.random() * 5) + 2;
+            let experienceCount = Math.floor(this.experience / randomCount);
+            console.log('+', randomCount);
+            let i = -Math.floor(randomCount / 2)
+            for (i; i < randomCount -Math.floor(randomCount / 2) - 1; i++)
+            {
+                let experience = new Experience(this.sprite.x + i * 20, this.collideBottom - 5, experienceCount);
+                experience.view();
+                experiences.push(experience);
+            }
+            let experience = new Experience(this.sprite.x + i * 20, this.collideBottom - 5, this.experience - experienceCount * (randomCount - 1))
+            experience.view();
+            experiences.push(experience);
+        }
+        
+        this.update = function (time)
+        {
+            if (!this.dead)
+            {
+                if (this.currentTimeShotAttack > 0)
+                {
+                    this.currentTimeShotAttack -= 1 * time.deltaTime;
+                }
+                if (this.currentTimeUltaAttack > 0)
+                {
+                    this.currentTimeUltaAttack -= 1 * time.deltaTime;
+                }
+                // if (this.currentTimeSpawnDevils > 0)
+                // {
+                //     this.currentTimeSpawnDevils -= 1 * time.deltaTime;
+                // } 
+                this.updateHp();
+                this.updateCollide();
+                this.updateMove(time);
+                this.updateAggression();
+            }
+            else 
+            {
+                this.deadTime -= time.deltaTime;
+            }
+        }
+    }
+}
--- public/script/index.js
+++ public/script/index.js
@@ -12,6 +12,7 @@ const enemies = [];
 const poisons = [];
 const fires = [];
 const smokes = [];
+const shots = [];
 
 const app = new PIXI.Application();
 const GRAVITY_ACCELERATION = 0.98;
@@ -33,6 +34,9 @@ const fire_anim = [];
 const mushroom_idle = [];
 const mushroom_active = [];
 const smoke_anim = [];
+const boss_idle = [];
+const boss_walk = [];
+const boss_dead = [];
 
 const keys =
 {
@@ -192,8 +196,6 @@ function onKeyUp(event)
     await PIXI.Assets.load([
         { alias: 'level1_map', src: '/images/first_level_map.jpg' },
         { alias: 'level2_map', src: '/images/level2_map.png' },
-        { alias: 'sound_on', src: '/images/sound_on.png'},
-        { alias: 'sound_off', src: '/images/sound_off.png'},
         { alias: 'hero_idle_group', src: '/images/hero_idle_group.json' },
         { alias: 'hero_walk_group', src: '/images/hero_walk_group.json' },
         { alias: 'hero_jump_group', src: '/images/hero_jump_group.json' },
@@ -214,7 +216,10 @@ function onKeyUp(event)
         { alias: 'health', src: '/images/health.png' },
         { alias: 'portal', src: '/images/portal.png' },
         { alias: 'non_active_portal', src: '/images/non_active_portal.png' },
+        { alias: 'boss', src: '/images/boss.json' },
+        { alias: 'shot', src: '/images/shot.png' },
     ])
+
     for (let i = 0; i < 10; i++)
     {
         hero_idle.push(PIXI.Texture.from(`hero_idle${1 + i}.png`));
@@ -268,9 +273,18 @@ function onKeyUp(event)
     {
         smoke_anim.push(PIXI.Texture.from(`smoke${1 + i}.png`));
     }
-
-   
-    
+    for (let i = 0; i < 4; i++)
+    {
+        boss_idle.push(PIXI.Texture.from(`bossIdle${1 + i}.png`));
+    }
+    for (let i = 0; i < 8; i++)
+    {
+        boss_walk.push(PIXI.Texture.from(`bossWalk${1 + i}.png`));
+    }
+    for (let i = 0; i < 8; i++)
+    {
+        boss_dead.push(PIXI.Texture.from(`bossDead${1 + i}.png`));
+    }
 
     app.canvas.addEventListener('mousedown', onAppMouseDown);
     app.canvas.addEventListener('mousemove', onAppMouseMove);
@@ -319,6 +333,7 @@ function onKeyUp(event)
             i++;
         }
         i = 0;
+
         while (i < poisons.length)
         {
             if (poisons[i].lifeTime > 0)
@@ -330,14 +345,15 @@ function onKeyUp(event)
                 poisons[i].sprite.destroy();
                 poisons.splice(i, 1);
                 i--;
-                if (enemies.length === 0)  // проверка победы, если все убиты
-                {
-                    window.location.href = "/win";
-                }
+                // if (enemies.length === 0)  // проверка победы, если все убиты
+                // {
+                //     window.location.href = "/win";
+                // }
             }
             i++;
         }
         i = 0;
+
         while (i < experiences.length)
         {
             if (!experiences[i].isTaken)
@@ -354,6 +370,7 @@ function onKeyUp(event)
             i++;
         }
         i = 0;
+
         while (i < bullets.length)
         {
             if (bullets[i].lifeTime > 0)
@@ -369,7 +386,23 @@ function onKeyUp(event)
             i++;
         }
         i = 0;
-        i = 0;
+        
+        while (i < shots.length)
+        {
+            if (shots[i].lifeTime > 0)
+            {
+                shots[i].update(time);
+            }
+            else
+            {
+                shots[i].sprite.destroy();
+                shots.splice(i, 1);
+                i--;
+            }
+            i++;
+        }
+        
+        i = 0;    
         while (i < smokes.length)
         {
             if (smokes[i].lifeTime > 0)
@@ -384,6 +417,7 @@ function onKeyUp(event)
             }
             i++;
         }
+
         i = 0;
         while (i < fireballs.length)
         {
@@ -399,10 +433,12 @@ function onKeyUp(event)
             }
             i++;
         }
+
         for (let i = 0; i < fires.length; i++)
         {
             fires[i].update();
         }
+
         i = 0;
         while (i < arrayOfBonus.length)
         {
@@ -460,81 +496,6 @@ function moveCamera(x, y)
 
 window.addEventListener('keydown', onKeyDown);
 window.addEventListener('keyup', onKeyUp);
-//window.addEventListener('keypress', () => {console.log('press')})
-//window.addEventListener('dblclick', doubleClick);
-// function levelCreate()
-// {
-//     let texture //= PIXI.Texture.from('ground');
-//     platforms.push(new Ground(texture, 1500, 70, 3000, 40)); // потолок - 4 уровень
-//     platforms.push(new Ground(texture, 345, 255, 690, 40)); // 3 уровень
-//     woodenPlanks.push(new WoodenPlank(texture, 745, 255, 110, 40));// 3 уровень
-
-//     platforms.push(new Ground(texture, 345, 550, 690, 40)); // 2 уровень
-//     platforms.push(new Ground(texture, 1355, 550, 1110, 40)); // 2 уровень
-//     platforms.push(new Ground(texture, 2270, 550, 520, 40)); // 2 уровень
-//     platforms.push(new Ground(texture, 2850, 550, 300, 40)); // 2 уровень
-//     woodenPlanks.push(new WoodenPlank(texture, 745, 550, 110, 40));// 2 уровень
-//     woodenPlanks.push(new WoodenPlank(texture, 1958, 550, 100, 40));// 2 уровень
-
-
-//     platforms.push(new Ground(texture, 345, 845, 690, 40)); // 1 уровень
-//     platforms.push(new Ground(texture, 1085, 845, 570, 40)); // 1 уровень
-//     platforms.push(new Ground(texture, 1870, 845, 800, 40)); // 1 уровень
-//     platforms.push(new Ground(texture, 2685, 845, 630, 40)); // 1 уровень
-//     woodenPlanks.push(new WoodenPlank(texture, 745, 845, 110, 40));// 1 уровень
-//     woodenPlanks.push(new WoodenPlank(texture, 1420, 845, 100, 40));// 1 уровень
-//     woodenPlanks.push(new WoodenPlank(texture, 2320, 845, 100, 40));// 1 уровень
-
-//     platforms.push(new Ground(texture, 1500, 1155, 3000, 40)); // пол - 0 уровень
-
-//     arrayOfWall.push(new Wall(texture, 1262, 410, 64, 820));
-//     arrayOfWall.push(new Wall(texture, 1635, 1010, 64, 320));
-//     arrayOfWall.push(new Wall(texture, 2500, 265, 64, 530));
-//     arrayOfWall.push(new Wall(texture, 1828, 450, 68, 160)); // высокая тумба
-//     arrayOfWall.push(new Wall(texture, 2645, 790, 68, 60)); // низкая тумба
-//     arrayOfWall.push(new Wall(texture, 10, 150, 64, 150)); // левая стена
-//     arrayOfWall.push(new Wall(texture, 78, 730, 128, 1050)); // левая стена
-//     arrayOfWall.push(new Wall(texture, 2950, 625, 128, 1250)); // правая стена
-    
-    
-//     enemies.push(new Bat(300, 350, 200, 200, 400, 400));// bat test
-//     fires.push(new Fire(1000, 500));// fire test
-
-//     enemies.push(new Mushroom(1000, 350, 250, 220));// mushroom test
-
-//     enemies.push(new Devil(1600, 500, 300, 0, 300, 50, 130));// 2 уровень
-//     enemies.push(new Devil(1200, 500, 300, 0, 300, 50, 110));// 2 уровень
-
-//     enemies.push(new Devil(1600, 795, 300, 0, 300, 50, 90));// 1 уровень
-//     enemies.push(new Devil(1200, 795, 300, 0, 300, 50, 130));// 1 уровень
-
-//     enemies.push(new Devil(350, 1105, 300, 0, 300, 50, 100));// 0 уровень
-//     enemies.push(new Devil(1300, 1105, 150, 0, 300, 50, 120));// 0 уровень
-//     arrayOfWall.forEach(wall => {
-//         wall.view();
-//     })
-//     woodenPlanks.forEach(woodenPlank => {
-//         woodenPlank.view();
-//     }
-//     )
-    
-//     enemies.push(new Bat(300, 350, 200, 200, 400, 400));// bat test
-
-//     platforms.forEach(platform =>
-//     {
-//         platform.view();
-//     });
-//     enemies.forEach(enemy =>
-//     {
-//         enemy.view();
-//     });
-
-//     fires.forEach(fire =>
-//     {
-//         fire.view();
-//     });
-
-// }
 
 function addBackground(app) {
     const background = PIXI.Sprite.from('background');
