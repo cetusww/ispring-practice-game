8dd36bc883cc6240fbb9f4eb9b36090186763d1f
Ersulov <ersulov05@mail.ru>
2024-07-17 15:49:32 +0300

    andrei-07-15-multiplayer-test добавил работу состояний

--- public/script/Bullet.js
+++ public/script/Bullet.js
@@ -2,18 +2,20 @@ class Bullet
 {
     constructor(texture, posX, posY, vecX, vecY, angle)
     {
-        this.sprite = new PIXI.Sprite(PIXI.Texture.from(texture));
+        this.sprite = new PIXI.Sprite(PIXI.Texture.from('bullet'));
         this.sprite.x = posX;
         this.sprite.y = posY;
         this.sprite.width = 12;
         this.sprite.height = 17;
+        this.angle = angle
         this.sprite.anchor.set(0.5);
         this.speed = 10;
         this.sprite.vx = vecX * this.speed;
         this.sprite.vy = vecY * this.speed;
         this.sprite.rotation += 1.57;
-        this.sprite.rotation += angle;
+        this.sprite.rotation += this.angle;
         this.lifeTime = 0.6 * FPS;
+        this.id = (new Date().getTime) * Math.random();
         this.boom = false;
 
         this.view = function ()
@@ -25,7 +27,33 @@ class Bullet
         {
             scene.removeChild(this.sprite);
         }
-
+        this.updateCollideHero = function (time)
+        {
+            if (this.lifeTime > 0 && !this.boom) {
+                if (hero.collideLeft <= this.sprite.x && hero.collideRight >= this.sprite.x &&
+                    hero.collideBottom >= this.sprite.y && hero.collideTop <= this.sprite.y
+                ) {
+                    this.lifeTime = 0;
+                    this.boom = true;
+                    hero.takeDamage(10);
+                }
+            }
+            this.lifeTime -= time.deltaTime;
+        }
+        this.updateCollideOpponent = function (opponent)
+        {
+            if (this.lifeTime > 0 && !this.boom) {
+                if (opponent.collideLeft <= this.sprite.x && opponent.collideRight >= this.sprite.x &&
+                    opponent.collideBottom >= this.sprite.y && opponent.collideTop <= this.sprite.y
+                ) {
+                    this.lifeTime = 0;
+                    this.boom = true;
+                    opponent.takeDamage(10);
+                    return 10;
+                }
+            }
+            return 0
+        }
         this.update = function (time)
         {
             if (this.lifeTime > 0 && !this.boom)
@@ -33,7 +61,21 @@ class Bullet
                 this.sprite.x += this.sprite.vx * time.deltaTime;
                 this.sprite.y += this.sprite.vy * time.deltaTime;
                 
-
+                // for (let i = 0; i < arrayOfOpponent.length; i++)
+                // {
+                //     let opponent = arrayOfOpponent[i];
+                //     if (this.sprite.y <= opponent.collideBottom &&
+                //         this.sprite.y >= opponent.collideTop &&
+                //         this.sprite.x <= opponent.collideRight &&
+                //         this.sprite.x >= opponent.collideLeft
+                //     )
+                //     {
+                //         this.boom = true;
+                //         this.lifeTime = 2;
+                //         //opponent.takeDamage(10);
+                //         break;
+                //     }
+                // }
                 for (let i = 0; i < enemies.length; i++)
                 {
                     let enemy = enemies[i];
