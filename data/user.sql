d23731500cdd7afee7acd17a1a741d625cdfc9c1
cetusw <kugelev.m@gmail.com>
2024-07-25 15:27:27 +0300

    misha-07-25-congrats-improvements обновил модель бд

--- data/user.sql
+++ data/user.sql
@@ -7,9 +7,14 @@ CREATE TABLE user
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    level int DEFAULT NULL,
-    score_1 int DEFAULT NULL,
-    score_2 int DEFAULT NULL,
-    score_3 int DEFAULT NULL,
+    level int DEFAULT 0,
+    score_1 int DEFAULT 0,
+    score_2 int DEFAULT 0,
+    score_3 int DEFAULT 0,
+    multiplayer_all int DEFAULT 0,
+    multiplayer_win int DEFAULT 0,
+    time_1 float DEFAULT 0,
+    time_2 float DEFAULT 0,
+    time_3 float DEFAULT 0,
     UNIQUE (username)
 );
